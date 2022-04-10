ff98d1b7db297b28324637a50ef4c7776065c97d
Njoki <kinuthiash@gmail.com>
2022-04-10 13:48:55 +0300

    commit tree 938f21d3be32dc5806dfbe506379021006ba30a5

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+// global scope variable
 var customerName = 'bob'
-console.log(customerName);
+
+// uppercase function
+function upperCaseCustomerName(){
+ return customerName = customerName.toUpperCase();
+}
+
+
+// best customer
+
+function setBestCustomer(){ 
+  
+  return bestCustomer  = 'not bob';
+}
+
+
+// change best customer - works
+function overwriteBestCustomer(){
+  return bestCustomer = "maybe bob"
+}
+
+
+// try changing variable declared by const - works
+const leastFavoriteCustomer = "Wayne Dyer"
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = "Esther Hicks"
+}
